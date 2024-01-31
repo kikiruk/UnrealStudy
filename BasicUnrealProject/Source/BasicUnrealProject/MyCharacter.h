dd6f403ab874b814ef23b58fa2e7fcd3c18611a3
seagull <33773761+kikiruk@users.noreply.github.com>
2024-02-01 02:29:18 +0900

    MyChracter 생성 , MyGameModeBase 생성
    MyCharacter 생성 후 skeletalmesh 적용,
    MyGameModeBase 생성 후 Ddfault PawnClass 를 MyCharacter 로 적용하고 worldSetting 에 적용

---
+++ BasicUnrealProject/Source/BasicUnrealProject/MyCharacter.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class BASICUNREALPROJECT_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+	//UPROPERTY()
+
+	UPROPERTY(EditAnywhere)
+	float TestFloat;
+
+	UPROPERTY(EditAnywhere)
+	FVector TestVector;
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
