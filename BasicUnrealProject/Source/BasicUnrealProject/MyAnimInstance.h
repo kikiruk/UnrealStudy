b527c42ad5489493bad026d8ece58125c5747aa9
seagull <33773761+kikiruk@users.noreply.github.com>
2024-02-12 21:30:29 +0900

    MyAnimInstance 생성, 맴버변수 생성
    MyAnimInstance 생성, 맴버변수 생성
    UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true));
    지정자를 통해서,
    카테고리 Animation 에 생성하였으며,
    AllowPrivateAccess 는 private 여도 블루프린트에서 접근 가능하도록 만들어준다.

---
+++ BasicUnrealProject/Source/BasicUnrealProject/MyAnimInstance.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICUNREALPROJECT_API UMyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+private:
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true));
+	float Speed;
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true));
+	float Horizontal;
+	UPROPERTY(Category = "Animation", EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true));
+	float Vertical;
+};
